5da194488664b7666ac6fbbf4060b68873330e86
TreyPoynter <treypoynter@gmail.com>
2023-12-18 12:04:12 -0600

    no passport implementation

--- server.js
+++ server.js
@@ -4,16 +4,11 @@ import { UserRouter } from "./routes/apis/user.js";
 import { BugRouter } from "./routes/apis/bugs.js";
 import { CommentRouter } from "./routes/apis/comments.js";
 import { TestRouter } from "./routes/apis/test.js";
-import { addNewUser, getUserByGithubId } from "./database.js";
 import express from 'express';
 import cors from 'cors'
 import cookieParser from "cookie-parser";
 import { authMiddleware } from "@merlin4/express-auth";
-import passport from 'passport';
 import session from 'express-session';
-import { createProxyMiddleware } from 'http-proxy-middleware'
-import { Strategy as GitHubStrategy } from 'passport-github2';
-import './routes/apis/auth.js';
 
 
 dotenv.config();
@@ -22,8 +17,6 @@ const app = express();
 app.use(cookieParser());
 app.use(express.static('public'));
 app.use(session({ secret: process.env.JWT_SECRET, resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 const debugServer = debug('app:Server');
 app.use(express.json())
 app.use(cors({
@@ -31,37 +24,6 @@ app.use(cors({
     credentials: true
 }));
 const port = process.env.PORT || 5001;
-
-//? START OF GITHUB OAUTH
-
-passport.use(new GitHubStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: `/auth/github/callback`,
-    state: true
-},
-    function (accessToken, refreshToken, profile, done) {
-        debugServer(profile)
-        return done(null, profile);
-    }
-));
-
-app.get('/auth/github',
-    passport.authenticate('github', { scope: ['user:email'] }));
-
-app.get('/auth/github/callback',
-    passport.authenticate('github', { failureRedirect: '/login' }),
-    function (req, res) {
-        // Successful authentication, redirect home.
-        res.redirect('/');
-    });
-passport.serializeUser(function (user, done) {
-    done(null, user.id);
-});
-passport.deserializeUser(function (user, done) {
-    return done(null, user);
-});
-//? END OF GITHUB OAUTH
 
 app.use(authMiddleware(process.env.JWT_SECRET, 'authToken', {
     httpOnly: true,
